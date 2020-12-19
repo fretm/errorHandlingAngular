5e341aa5d0c89fe0a12752962009aee81ec6c8c1
frey <frehiwottm@gmail.com>
2020-12-18 16:23:41 -0800

    feature

---
+++ src/app/feature/sub.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub',
+  template: `
+    <p>
+      sub works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
